9390abf72567039af93ebfe79918dfb972e4f791
Tristan Young <tristan.young@verizon.com>
2019-07-31 16:04:03 -0500

    update

--- README.md
+++ README.md
@@ -26,6 +26,4 @@ This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md
 
 ## Acknowledgments
 
-* Hat tip to anyone whose code was used
-* Inspiration
-* etc
+* To my coach and mentor Bill Allen
--- app.js
+++ app.js
@@ -1,15 +1,45 @@
+/*
+* the express variable gives access to the express API
+* require express and data.json
+*/
 const express = require('express');
-
+//new instance of express. function declaration
 const app = express();
 
-const data = ("./data.json");
+const data = require('./data.json');
+//set up pug to view engine
+app.set('view engine', 'pug');
+//set route to index page using render
+app.get('/', (req, res) =>{
+    res.render('index', {projects:data.projects}); 
+});
+//set the rout to about page using render
+app.get('/about', (req, res) =>{
+     res.render('about'); 
+});
+//set route to project
+app.get('/projects/:id', (req, res) =>{
+    res.render('project', {
+        project: data.projects[req.params.id - 1],
+        id: req.params.id
+    }); 
+});
+app.use('/static', express.static('public'));
 
-app.set("view engine", "pug");
+//catching error using next to forward error to error handler
+app.use((req, res, next) => {
+    const err=new Error("This page does not exist");
+    err.status=404;
+    next(err);
+ });
 
-app.use("/static", express.static(path.join(_dirname, "public")));
+// set route to error page using rendor
+  app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.render('error', err);
+    console.log("This page does not exist");
+      });
 
-app.get('/', function (req, res) {
-    res.render('index')
-  })
-  
-  app.listen(3000)
+//app will listen on port 3000 - localhost:3000
+ app.listen(3000, () => {
+ console.log('Listening on port 3000')});
